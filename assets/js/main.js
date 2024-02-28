a68e31f80f449ccd915f039695ca558b101d0c65
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-28 16:40:16 +0100

    Add grid 10x10 dinamically

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-console.log('Camominato');
+console.log('Campominato');
+// Creo le celle
+const gameGrid = document.querySelector('.game_container');
+// console.log(gameGrid);
+const markUpCell = '<div class="cell"></div>'
+const totalCells = 100;
+
+for (let i = 1; i <= totalCells; i++) {
+    const cell = markUpCell
+    console.log(cell);
+    gameGrid.insertAdjacentHTML("afterbegin", cell)
+}
