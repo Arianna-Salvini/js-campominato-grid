f81f94fa828b34e7896db04e7d320b20830d2890
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-28 17:55:47 +0100

    Add sequential number in cell Fix function and delete function for array of numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,23 @@
 console.log('Campominato');
 
 const totalCells = 100;
-
-
+// Creo la griglia di gioco
 /**
- * Create a grid dinamically in DOM
+ * Create a grid dinamically with sequential number in it in DOM
  * @param {Number} 
  */
 function creatGameGrid(totalCells) {
     // Creo le celle
     const gameGrid = document.querySelector('.game_container');
     // console.log(gameGrid);
-    const markUpCell = '<div class="cell"></div>'
-
+    
     for (let i = 1; i <= totalCells; i++) {
+        let cellNumber = i
+        const markUpCell = `<div class="cell">${cellNumber}</div>`
         const cell = markUpCell
         console.log(cell);
-        gameGrid.insertAdjacentHTML("afterbegin", cell)
+        gameGrid.insertAdjacentHTML("beforeend", cell)
+
     }
 }
-
 creatGameGrid(totalCells)
-
-// Creo una sequenza di numeri da 1 a 100
-
-
-const totalCellNumber = 100
-
-
-function cellNumberSEquence(totalCellNumber) {
-    const cellNumber = []
-    for (let i = 1; i < totalCellNumber + 1; i++) {
-        cellNumber.push(i)
-    }
-    return cellNumber
-}
-console.log(cellNumberSEquence(totalCellNumber));
