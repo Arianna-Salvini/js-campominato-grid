c0e2d8d1cc0fe3a1cabc8d22143b66c74c064cff
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-28 17:02:29 +0100

    Add function to creat an array of sequential number

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,25 +7,32 @@ const totalCells = 100;
  * Create a grid dinamically in DOM
  * @param {Number} 
  */
-function creatGameGrid (totalCells){
+function creatGameGrid(totalCells) {
     // Creo le celle
-const gameGrid = document.querySelector('.game_container');
-// console.log(gameGrid);
-const markUpCell = '<div class="cell"></div>'
+    const gameGrid = document.querySelector('.game_container');
+    // console.log(gameGrid);
+    const markUpCell = '<div class="cell"></div>'
 
-for (let i = 1; i <= totalCells; i++) {
-    const cell = markUpCell
-    console.log(cell);
-    gameGrid.insertAdjacentHTML("afterbegin", cell)
-}
+    for (let i = 1; i <= totalCells; i++) {
+        const cell = markUpCell
+        console.log(cell);
+        gameGrid.insertAdjacentHTML("afterbegin", cell)
+    }
 }
 
 creatGameGrid(totalCells)
 
-const cellNumber = []
+// Creo una sequenza di numeri da 1 a 100
 
-for (let i = 1; i < 101; i++) {
-    cellNumber.push(i)
+
+const totalCellNumber = 100
+
+
+function cellNumberSEquence(totalCellNumber) {
+    const cellNumber = []
+    for (let i = 1; i < totalCellNumber + 1; i++) {
+        cellNumber.push(i)
+    }
+    return cellNumber
 }
-
-// console.log(cellNumber);
+console.log(cellNumberSEquence(totalCellNumber));
