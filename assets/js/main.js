04fc52b075e95ea70a289f7028e87c9c826752ce
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-28 16:53:07 +0100

    Add array with sequential nujmber from 1 to 100

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,31 @@
 console.log('Campominato');
-// Creo le celle
+
+const totalCells = 100;
+
+
+/**
+ * Create a grid dinamically in DOM
+ * @param {Number} 
+ */
+function creatGameGrid (totalCells){
+    // Creo le celle
 const gameGrid = document.querySelector('.game_container');
 // console.log(gameGrid);
 const markUpCell = '<div class="cell"></div>'
-const totalCells = 100;
 
 for (let i = 1; i <= totalCells; i++) {
     const cell = markUpCell
     console.log(cell);
     gameGrid.insertAdjacentHTML("afterbegin", cell)
 }
+}
+
+creatGameGrid(totalCells)
+
+const cellNumber = []
+
+for (let i = 1; i < 101; i++) {
+    cellNumber.push(i)
+}
+
+// console.log(cellNumber);
