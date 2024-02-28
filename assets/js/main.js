5424b52b1f9570cb61ce5f50b4ce70c635ba37a8
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-28 23:24:17 +0100

    Fix cicle for and add event on cell to color them when you click on them

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,32 +3,34 @@ console.log('Campominato');
 const totalCells = 100;
 
 const gameGrid = document.querySelector('.game_container');
-gameGrid.classList.add('hidden')
+gameGrid.classList.add('hidden');
 
-const btnStart = document.getElementById("start_game")
-
+const btnStart = document.getElementById("start_game");
+// bottone start
 btnStart.addEventListener('click', function () {
-    gameGrid.classList.toggle('hidden')
-
+    gameGrid.classList.toggle('hidden');
     // Creo la griglia di gioco
     /**
      * Create a grid dinamically with sequential number in it in DOM
      * @param {Number} 
      */
     function creatGameGrid(totalCells) {
-
         // Creo le celle
         // console.log(gameGrid);
-
         for (let i = 1; i <= totalCells; i++) {
             let cellNumber = i
-            const markUpCell = `<div class="cell">${cellNumber}</div>`
-            const cell = markUpCell
-            console.log(cell);
-            gameGrid.insertAdjacentHTML("beforeend", cell)
+            // const markUpCell = `<div class="cell"> ${cellNumber}</div>`;
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.textContent = cellNumber
+            gameGrid.appendChild(cell)
 
+            cell.addEventListener('click', function () {
+                cell.classList.toggle('clicked');
+                console.log(cellNumber);
 
-        }
+        });
     }
-    creatGameGrid(totalCells)
-})
+}
+creatGameGrid(totalCells)
+});
