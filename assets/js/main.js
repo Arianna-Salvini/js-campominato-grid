b19e12df6edcda697bac04aa8a2f75a6a895893b
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-28 18:34:19 +0100

    Add click event to button in header to make visible the grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,34 @@
 console.log('Campominato');
 
 const totalCells = 100;
-// Creo la griglia di gioco
-/**
- * Create a grid dinamically with sequential number in it in DOM
- * @param {Number} 
- */
-function creatGameGrid(totalCells) {
-    // Creo le celle
-    const gameGrid = document.querySelector('.game_container');
-    // console.log(gameGrid);
-    
-    for (let i = 1; i <= totalCells; i++) {
-        let cellNumber = i
-        const markUpCell = `<div class="cell">${cellNumber}</div>`
-        const cell = markUpCell
-        console.log(cell);
-        gameGrid.insertAdjacentHTML("beforeend", cell)
 
+const gameGrid = document.querySelector('.game_container');
+gameGrid.classList.add('hidden')
+
+const btnStart = document.getElementById("start_game")
+
+btnStart.addEventListener('click', function () {
+    gameGrid.classList.toggle('hidden')
+
+    // Creo la griglia di gioco
+    /**
+     * Create a grid dinamically with sequential number in it in DOM
+     * @param {Number} 
+     */
+    function creatGameGrid(totalCells) {
+
+        // Creo le celle
+        // console.log(gameGrid);
+
+        for (let i = 1; i <= totalCells; i++) {
+            let cellNumber = i
+            const markUpCell = `<div class="cell">${cellNumber}</div>`
+            const cell = markUpCell
+            console.log(cell);
+            gameGrid.insertAdjacentHTML("beforeend", cell)
+
+
+        }
     }
-}
-creatGameGrid(totalCells)
+    creatGameGrid(totalCells)
+})
