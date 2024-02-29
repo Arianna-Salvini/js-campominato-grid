eb9ae8226a14d0d97453db6f0551642092eb9f7c
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-29 14:54:40 +0100

    Add condition to win or lose

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,20 @@ gameGrid.classList.add('hidden');
 
 const btnStart = document.getElementById("start_game");
 // add const for maximum number of asteroid and an array for the number cell where asteroids is
-const asteroidList = []
-const maxAsteroidNumb = 16
+const asteroidList = [];
+const maxAsteroidNumb = 16;
 
+// add a constant for maximum cells clickable excluding astroid cells --- condition for win
+const maxFreeCell = totalCells - maxAsteroidNumb;
+const cellClicked = [];
+// console.log(maxFreeCell);
 // bottone start
 btnStart.addEventListener('click', function () {
     gameGrid.classList.add('hidden');
-    gameGrid.innerHTML = ''
-    createGameGrid(totalCells)
-    genrateRandomASteroid()
-    console.log(genrateRandomASteroid()); 
+    gameGrid.innerHTML = '';
+    createGameGrid(totalCells);
+    genrateRandomASteroid();
+    console.log(genrateRandomASteroid());
 });
 
 // Creo la griglia di gioco
@@ -27,38 +31,45 @@ btnStart.addEventListener('click', function () {
 function createGameGrid(totalCells) {
     // Creo le celle
     // console.log(gameGrid);
-    let cell
+    let cell;
     for (let i = 1; i <= totalCells; i++) {
-        let cellNumber = i
+        let cellNumber = i;
         // const markUpCell = `<div class="cell"> ${cellNumber}</div>`;
         const cell = document.createElement('div');
         cell.classList.add('cell');
-        cell.textContent = cellNumber
-        gameGrid.appendChild(cell)
+        cell.textContent = cellNumber;
+        gameGrid.appendChild(cell);
 
         cell.addEventListener('click', function () {
             cell.classList.add('clicked');
-            console.log(cellNumber);
-
-        // confonto i numeri delle celle coi numeri degli asteroidi
+            // console.log(cellNumber);
+            // add clicked cell into array
+            if (!cellClicked.includes(i)) {
+                cellClicked.push(i);
+            }
+            console.log(cellClicked);
+            // confonto i numeri delle celle coi numeri degli asteroidi
+            // add win condition
+            if (cellClicked.length === maxFreeCell) {
+                isWin(true)
+            }
 
             if (asteroidList.includes(i)) {
-                cell.classList.add('asteroid')
-                cell.textContent = '☄️'
+                cell.classList.add('asteroid');
+                cell.textContent = '☄️';
+                isWin(false);
             }
+
+
+
         });
-
-
-       
-
     }
     return gameGrid
 }
 
-
-function genrateRandomASteroid (){
-     // Genero 16 numeri randomici per gli asteroi e li push nel suo array
-     while (asteroidList.length < maxAsteroidNumb) {
+function genrateRandomASteroid() {
+    // Genero 16 numeri randomici per gli asteroi e li push nel suo array
+    while (asteroidList.length < maxAsteroidNumb) {
         const asteroids = Math.floor(Math.random() * totalCells) + 1;
         if (!asteroidList.includes(asteroids)) {
             asteroidList.push(asteroids);
@@ -67,4 +78,14 @@ function genrateRandomASteroid (){
     return asteroidList;
 }
 
+// condition to win or loose the game and stop it
 
+function isWin(winner) {
+    if (winner === true) {
+        return console.log('You win');
+    } else {
+        return console.log('You lose');
+    }
+}
+// console.log(isWin(true))
+// console.log(isWin(false))
